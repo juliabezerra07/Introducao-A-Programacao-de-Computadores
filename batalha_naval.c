246476c424644aef4f473c9729d0eeb3ca3f4bcd
júlia bezerra <bezerrajulia2015@gmail.com>
2025-10-25 22:20:22 -0300

    Update batalha_naval.c

--- batalha_naval.c
+++ batalha_naval.c
@@ -5,8 +5,8 @@ int main() {
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int linha[10]  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    int tabuleiro[10][10]; // por isso dava erro
-
+    int tabuleiro[10][10];
+    
     // tabuleiro
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
